ccd49b0f9116a2616514a43641c93482fb7225a9
Felipe Olavarria <178.felipe@gmail.com>
2020-07-26 22:05:29 -0400

    Add home page to router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages/home'
 
 function App() {
     return (
-        <div className="App">
-            H1
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </Router>
     )
 }
 
