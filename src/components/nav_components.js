06e1bd82e70b3b5f5d951f84db840a296923ffc3
Felipe Olavarria <178.felipe@gmail.com>
2020-07-27 21:23:17 -0400

    Delete semicolons from nav components.js

--- src/components/nav_components.js
+++ src/components/nav_components.js
@@ -6,12 +6,12 @@ const NavTitle = styled.div`
     padding-right: 3rem;
     font-size: 1.8rem;
     color: ${props => props.color ? props.color : "black"};
-`;
+`
 
 const NavBar = styled.nav`
     padding: 1rem 0;
     background-color: dodgerblue;
-`;
+`
 
 const NavList = styled.ul`
     display: inline-block;
@@ -19,17 +19,17 @@ const NavList = styled.ul`
     margin: 0;
     padding: 0;
     padding-left: 2rem;
-`;
+`
 
 const NavItem = styled.li`
     float: left;
     padding: .5rem 2rem;
-`;
+`
   
 const NavLink = styled(Link)`
     text-decoration: none;
     font-size: 1.2rem;
     color: white;
-`;
+`
 
 export { NavBar, NavTitle, NavList, NavItem, NavLink }
