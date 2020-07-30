3b6c90fc47146bce1caf4c10ddcb4e9eddd44d91
Felipe Olavarria <178.felipe@gmail.com>
2020-07-30 19:58:35 -0400

    Add budgets page skeleton

---
+++ src/pages/budgets.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Budgets = () => {
+    return (
+        <Layout>
+            Budgets
+        </Layout>
+    )
+}
+
+export default Budgets
