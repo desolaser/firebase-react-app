d2000af0381c97bcc2fa08569f007cdd6cffefec
Felipe Olavarria <178.felipe@gmail.com>
2020-07-30 19:03:18 -0400

    Create auth context

---
+++ src/auth/context.js
@@ -0,0 +1,8 @@
+import React, { useContext } from 'react'
+const AuthContext = React.createContext(null)
+
+const useAuth = () => {
+    return useContext(AuthContext)
+}
+
+export default AuthContext
