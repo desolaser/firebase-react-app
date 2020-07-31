ac2e2057db878ef5260138a717e502d39307eda9
Felipe Olavarria <178.felipe@gmail.com>
2020-07-30 20:07:52 -0400

    Add products page skeleton

---
+++ src/pages/products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Products = () => {
+    return (
+        <Layout>
+            Products
+        </Layout>
+    )
+}
+
+export default Products
