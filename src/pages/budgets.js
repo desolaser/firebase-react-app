b5a0c9891a328b4f6cbbccb54ade51f6b66b21e0
Felipe Olavarria <178.felipe@gmail.com>
2020-08-01 16:11:56 -0400

    Add budget list to budgets page

--- src/pages/budgets.js
+++ src/pages/budgets.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useFirebase } from '../firebase'
+
 import Layout from '../components/layout'
+import StyledList from '../components/styled_list'
 
 const Budgets = () => {
+    const firebase = useFirebase()
+
+    const [ budgets, setBudgets ] = useState([])
+    const [ loading, setLoading ] = useState(true)
+
+    useEffect(() => {
+        firebase.budgets().on('value', snapshot => {
+            const budgetsObject = snapshot.val()
+            const budgetsList = Object.keys(budgetsObject).map(key => ({
+                uid: key,
+                ...budgetsObject[key],
+            }))
+    
+            setBudgets(budgetsList)
+            setLoading(false)
+        })
+
+        return () => firebase.budgets().off()
+    })
+
     return (
         <Layout>
-            Budgets
+            <h1>Budgets</h1>
+            {loading && <div>Loading ...</div>} 
+            <StyledList data={budgets} />
         </Layout>
     )
 }
