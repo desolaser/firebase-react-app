86acccebcaae3665c2d5f30077cfb1a707404b7b
Felipe Olavarria <178.felipe@gmail.com>
2020-08-01 16:12:05 -0400

    add products list to products page

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useFirebase } from '../firebase'
+
 import Layout from '../components/layout'
+import StyledList from '../components/styled_list'
 
 const Products = () => {
+    const firebase = useFirebase()
+
+    const [ products, setProducts ] = useState([])
+    const [ loading, setLoading ] = useState(true)
+
+    useEffect(() => {
+        firebase.products().on('value', snapshot => {
+            const productsObject = snapshot.val()
+            const productsList = Object.keys(productsObject).map(key => ({
+                uid: key,
+                ...productsObject[key],
+            }))
+    
+            setProducts(productsList)
+            setLoading(false)
+        })
+
+        return () => firebase.products().off()
+    })
+
     return (
         <Layout>
-            Products
+            <h1>Products</h1>
+            {loading && <div>Loading ...</div>} 
+            <StyledList data={products} />
         </Layout>
     )
 }
