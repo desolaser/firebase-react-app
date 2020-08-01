8da778b76be59dd1dc5b0c9d1fd2f5be9696bf2f
Felipe Olavarria <178.felipe@gmail.com>
2020-08-01 15:50:36 -0400

    Fix return statement on useEffect in admin.js

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -10,25 +10,36 @@ const Admin = () => {
 
     useEffect(() => {
         firebase.users().on('value', snapshot => {
-            const usersObject = snapshot.val();
+            const usersObject = snapshot.val()
             const usersList = Object.keys(usersObject).map(key => ({
                 ...usersObject[key],
                 uid: key,
-            }));
-
+            }))
+    
             setUsers(usersList)
             setLoading(false)
         })
 
-        return (
-            firebase.users().off()
-        )
-    }, [])
+        return () => firebase.users().off()
+    })
+
 
     return (
         <Layout>
             <h1>Admin</h1>
-
+            {loading && <div>Loading ...</div>} 
+            <ul>
+                {users.map(user => (
+                    <li key={user.uid}>
+                        <span>
+                            <strong>ID:</strong> {user.uid}
+                        </span>
+                        <span>
+                            <strong>E-Mail:</strong> {user.email}
+                        </span>
+                    </li>
+                ))}
+            </ul>
         </Layout>
     )
 }
