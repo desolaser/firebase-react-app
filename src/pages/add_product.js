9f1e3fb3b3833ff73d0741ac30a1f0fc7fe7009e
Felipe Olavarria <178.felipe@gmail.com>
2020-08-02 13:51:47 -0400

    Create add_product page

---
+++ src/pages/add_product.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { useFirebase } from '../firebase'
+ 
+import Layout from '../components/layout'
+import Form from '../components/form'
+import Input from '../components/input'
+
+const AddProduct = () => {
+    const firebase = useFirebase()
+    const [ name, setName ] = useState('')
+    const [ price, setPrice ] = useState(0)
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        firebase.products().push({
+            name,
+            price
+        })
+        alert('Product added')
+    }
+
+    const isInvalid = name === '' || price === 0
+
+    const fields = [
+        {
+            label: "Name",
+            type: "text",
+            value: name,
+            onChange: e => setName(e.target.value)
+        },
+        {
+            label: "Price",
+            type: "number",
+            value: price,
+            onChange: e => setPrice(e.target.value)
+        },
+    ]
+
+    return (
+        <Layout>
+            <Form 
+                title="Product form"
+                fields={fields}
+                handleSubmit={handleSubmit}
+                submitValue="Submit"
+                disabled={isInvalid}
+            />
+        </Layout>
+    )
+}
+
+export default AddProduct
