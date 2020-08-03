6f78fa456690f65b88950e768054be58eea1225e
Felipe Olavarria <178.felipe@gmail.com>
2020-08-03 17:23:58 -0400

    Add style for products list inside budgets lists

--- src/components/budgets_list.js
+++ src/components/budgets_list.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useFirebase } from '../firebase'
 import {
     StyledUl,
@@ -7,6 +8,14 @@ import {
     StyledField,
     StyledButton
 } from './styled_list'
+
+const StyledProducts = styled.div`
+    margin-left: 2rem;
+`
+
+const StyledProductItem = styled.div`
+    margin-left: 4rem;
+`
 
 const BudgetsList = ({ data, history }) => {
     const firebase = useFirebase()
@@ -24,11 +33,40 @@ const BudgetsList = ({ data, history }) => {
         <StyledUl>
             {data.map(item => (
                 <StyledLi key={item.id}>
-                    {Object.entries(item).map((itemData, index) => (
-                        <StyledElement key={index}>
-                            <StyledField>{itemData[0]}: </StyledField>{" " + itemData[1]}
-                        </StyledElement>
-                    ))}
+                    {Object.entries(item).map((itemData, index) => 
+                        itemData[0] === "products" 
+                            ? (
+                                <div>
+                                    <StyledElement key={index}>
+                                        <StyledField>products</StyledField>
+                                    </StyledElement>
+                                    <StyledProducts>
+                                    {itemData[1].map((product, index) => (
+                                        <div>
+                                            {`Product ${index + 1}`} 
+                                            <StyledProductItem key={product.id + 1}>
+                                                <StyledField>Id: </StyledField>{" " + product.id}
+                                            </StyledProductItem>
+                                            <StyledProductItem key={product.id + 2}>
+                                                <StyledField>Name: </StyledField>{" " + product.name}
+                                            </StyledProductItem>
+                                            <StyledProductItem key={product.id + 3}>
+                                                <StyledField>Price: </StyledField>{" " + product.price}
+                                            </StyledProductItem>
+                                            <StyledProductItem key={product.id + 4}>
+                                                <StyledField>Quantity: </StyledField>{" " + product.quantity}
+                                            </StyledProductItem>
+                                        </div>
+                                    ))}
+                                    </StyledProducts> 
+                                </div>                             
+                            )
+                            : (
+                                <StyledElement key={index}>
+                                    <StyledField>{itemData[0]}: </StyledField>{" " + itemData[1]}
+                                </StyledElement>
+                            )
+                    )}
                     <StyledButton color="tomato" textColor="white" onClick={() => deleteBudgets(item.id)}>Delete</StyledButton>
                     <StyledButton color="dodgerblue" textColor="white" onClick={() => editBudgets(item.id)}>Edit</StyledButton>
                 </StyledLi>
